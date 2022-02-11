55502a5aa3dfaa0e92a7d3a5a7822a972d5a3099
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-02-11 17:07:41 +0100

    commit1.32
    ajout d'une nav bar

--- index.php
+++ index.php
@@ -2,13 +2,36 @@
 <html>
     <head>
     <title>Accueil</title>
-    <link href="main.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
 
     </head>
 
     <body>
+    <nav>
+        <!-- nav permet de mettre un ensemble de lien de navigation-->
+        <ul>
+            <li><a class="nav-color" href="index.php">Accueil</a>
+                <!-- mène à l'accueil / la class nav-color permet de mettre le texte en blanc-->
+            </li>
+            <li>
+                <a class="nav-color" href="pizza.php">Pizza</a>
+                <!-- mène à la page des pizza-->
+            </li>
+            <li>
+                <a class="nav-color" href="contact.html">A propos</a>
+                <!-- mène à la page A propos-->
+            </li>
+            <li>
+                <a class="nav-color" href="conexion.php">Conexion</a>
+                <!-- mène à la page de conexion-->
+            </li>
+            
+            
+            
+        </ul>
+    </nav>
         <div>
             
         </div>
