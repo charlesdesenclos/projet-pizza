d7c834b32848077a57a79e27a01180c916831caa
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-02-12 16:31:01 +0100

    commit 1.33
    ajout d'un prototype pour choisir une pizza

---
+++ bellacho_pizza.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Accueil</title>
+    <link href="style.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+
+    </head>
+
+    <body>
+    <nav>
+        <!-- nav permet de mettre un ensemble de lien de navigation-->
+        <ul>
+            <li><a class="nav-color" href="index.php">Accueil</a>
+                <!-- mène à l'accueil / la class nav-color permet de mettre le texte en blanc-->
+            </li>
+            <li>
+                <a class="nav-color" href="pizza.php">Pizza</a>
+                <!-- mène à la page des pizza-->
+            </li>
+            <li>
+                <a class="nav-color" href="contact.html">A propos</a>
+                <!-- mène à la page A propos-->
+            </li>
+            <li>
+                <a class="nav-color" href="conexion.php">Conexion</a>
+                <!-- mène à la page de conexion-->
+            </li>
+            
+            
+            
+        </ul>
+    </nav>
+    
+        <center><img src="images/bellacho_pizza.jpg" alt="bellacho_pizza" height="450px"></center>
+    
+
+    <div class="page">
+    <h1 role="heading" itemprop="name" class="espace"> 
+        Bellacho Pizza
+    </h1>
+    <hr>
+        <div >
+            <h2 class="espace"> Les ingrédients :</h2>
+
+            Sauce tomate, mozarella, chrorizo, merguez, duo de poivrons.
+
+            <h2 class="espace"> Informations consommateurs </h2>
+
+            Tous nos produits sont bio ainsi que produits en France.
+
+            
+        </div>
+    </div>
+    
+        <center><input class="button" type="button" value="Commander"></center>
+    
+    
+    </body >
+</html>
