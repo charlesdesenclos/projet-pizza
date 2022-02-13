bc44910c645c0d939b484e88f045685508a84352
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-02-13 11:49:46 +0100

    commit1.35

--- diavola_pizza.php
+++ diavola_pizza.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-    <title>Accueil</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css" />
+    <head>
+    <title>Diavola Pepperoni Pizza</title>
+    <link href="style.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
 
-</head>
+    </head>
 
-<body>
+    <body>
     <nav>
         <!-- nav permet de mettre un ensemble de lien de navigation-->
         <ul>
@@ -29,14 +27,35 @@
                 <a class="nav-color" href="conexion.php">Conexion</a>
                 <!-- mène à la page de conexion-->
             </li>
-
-
-
+            
+            
+            
         </ul>
     </nav>
     
+        <center><img src="images/bellacho_pizza.jpg" alt="bellacho_pizza" height="450px"></center>
     
 
-</body>
+    <div class="page">
+    <h1 role="heading" itemprop="name" class="espace"> 
+        Diavola Pepperoni Pizza
+    </h1>
+    <hr>
+        <div >
+            <h2 class="espace"> Les ingrédients :</h2>
 
+            Sauce tomate bio, mozzarella, pepperoni, duo de poivrons, oignons, piments jalapeños.
+
+            <h2 class="espace"> Informations consommateurs </h2>
+
+            Tous nos produits sont bio ainsi que produits en France.
+
+            
+        </div>
+    </div>
+    
+        <center><input class="button" type="button" value="Commander"></center>
+    
+    
+    </body >
 </html>
