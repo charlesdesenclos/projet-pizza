4eabbc9418ae289a1752bd74ef247782a47faddc
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-02-13 13:40:48 +0100

    commit1.361

---
+++ bacon_pizza.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Bacon Groovy Pizza</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+
+    </head>
+
+    <body>
+    <nav>
+        <!-- nav permet de mettre un ensemble de lien de navigation-->
+        <ul>
+            <li><a class="nav-color" href="index.php">Accueil</a>
+                <!-- mène à l'accueil / la class nav-color permet de mettre le texte en blanc-->
+            </li>
+            <li>
+                <a class="nav-color" href="pizza.php">Pizza</a>
+                <!-- mène à la page des pizza-->
+            </li>
+            <li>
+                <a class="nav-color" href="contact.html">A propos</a>
+                <!-- mène à la page A propos-->
+            </li>
+            <li>
+                <a class="nav-color" href="conexion.php">Conexion</a>
+                <!-- mène à la page de conexion-->
+            </li>
+            
+            
+            
+        </ul>
+    </nav>
+    
+        <center><img src="images/bellacho_pizza.jpg" alt="bellacho_pizza" height="450px"></center>
+    
+
+    <div class="page">
+    <h1 role="heading" itemprop="name" class="espace"> 
+        Bacon Groovy Pizza
+    </h1>
+    <hr>
+        <div >
+            <h2 class="espace"> Les ingrédients :</h2>
+
+            Crème fraîche légère, mozzarella, poulet rôti, oignons, bacon, sauce barbecue.
+
+            <h2 class="espace"> Informations consommateurs </h2>
+
+            Tous nos produits sont bio ainsi que produits en France.
+
+            
+        </div>
+    </div>
+    
+        <center><input class="button" type="button" value="Commander"></center>
+    
+    
+    </body >
+</html>
