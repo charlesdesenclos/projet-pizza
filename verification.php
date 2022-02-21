155d1287b6390d8fc4890d4d1856e17e8b0577cd
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-02-21 19:15:31 +0100

    commit1.91

--- verification.php
+++ verification.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('connexionDB.php'); // Fichier PHP contenant la connexion de la BDD
+include 'connexionDB.php'; // Fichier PHP contenant la connexion de la BDD
  
     // S'il y a une session alors on ne retourne plus sur cette page
     if (isset($_SESSION['id'])){
