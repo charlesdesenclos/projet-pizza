0079b9993a1d60efa8ffea8f9732a5ee03332e43
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-04-09 10:33:44 +0200

    commit15.1

--- User.php
+++ User.php
@@ -7,9 +7,11 @@ class User
     private $adresse_;
     private $bancaire_;
 
-    public function __construct()
+    public function __construct($NewPseudo, $NewEmail, $NewPassword)
     {
-
+        $this -> pseudo_ = $NewPseudo;
+        $this-> email_ = $NewEmail;
+        $this -> password = $NewPassword;
     }
 
     public function getEmail()
