28dd162d183f02a539eaaafe0f4f0784a8e50173
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-04-16 19:28:07 +0200

    commit15.8

--- Pizzas.php
+++ Pizzas.php
@@ -12,9 +12,24 @@ class Pizzas{
 
     }
 
-    public function pizza()
+  
+    public function requet()
     {
-        
-    }
+        require_once 'config.php';
 
+        $sql = 'SELECT Pizza.pizza AS nompizza, Pizza.prix AS nomprix, utilisateurs.pseudo AS nom, panier.id_utilisateurs AS id FROM panier,Pizza,utilisateurs WHERE Pizza.id = panier.id_pizza AND utilisateurs.id = panier.id_utilisateurs ORDER BY panier.id DESC';
+        $RequetStatement = $bdd->query($sql);
+    }
+    public function affiche()
+    {
+        echo"<h2 clas='espace2'>Voici la liste des commandes :</h2>";
+        while($tab = $RequetStatement->fetch())
+        {
+            if($tab['id'] == $data['id'])
+            {
+                echo"<tr><td>{$tab['nompizza']}</td><td>{$tab['nomprix']} € </td><td>{$tab['nom']}</td></tr>\n";
+            } 
+        }
+    }
+}
 ?>
